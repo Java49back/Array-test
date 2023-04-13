eb2b4f46e61313f75d93d90193d36d22b3dca2d1
Denis Galper <denis_1293@hotmail.com>
2023-04-13 19:56:36 +0300

    array tests(junit)

--- java-oop-algorithms-master/src/telran/util/test/ArrayListTest.java
+++ java-oop-algorithms-master/src/telran/util/test/ArrayListTest.java
@@ -11,81 +11,80 @@ import telran.util.List;
 
 
 class ArrayListTest {
-List<Integer> list;
-Integer[] numbers = {10, -20, 7, 50, 100, 30};
-@BeforeEach
-void setUp() {
-	list = new ArrayList<>(1);
-	for( int i = 0; i < numbers.length; i++) {
-		list.add(numbers[i]);
-	}
-}
-//	@Test
-//	void testAdd() {
-//	assertTrue(list.add(numbers[0]));
-//	assertEquals(numbers.length + 1, list.size());
-//	}
-//	@Test
-//	void testAddIndex() {
-//		Integer [] expected0_500 = {500, 10, -20, 7, 50, 100, 30};
-//		Integer [] expected0_500_3_700 = {500, 10, -20, 700, 7, 50, 100, 30};
-//		Integer [] expected0_500_3_700_8_300 = {500, 10, -20, 700, 7, 50, 100, 30, 300};
-//		list.add(0, 500);
-//		runTest(expected0_500);
-//		list.add(3, 700);
-//		runTest(expected0_500_3_700);
-//		list.add(8, 300);
-//		runTest(expected0_500_3_700_8_300);
-//		
-//	}
-//	@Test
-//	void testRemoveIndex() {
-//		Integer [] expectedNo10 = { -20, 7, 50, 100, 30};
-//		Integer [] expectedNo10_50 = { -20, 7,  100, 30};
-//		Integer [] expectedNo10_50_30 = { -20, 7,  100};
-//		assertEquals(10, list.remove(0));
-//		runTest(expectedNo10);
-//		assertEquals(50, list.remove(2));
-//		runTest(expectedNo10_50);
-//		assertEquals(30, list.remove(3));
-//		runTest(expectedNo10_50_30);
-//		
-//	}
-//	@Test
-//	void testGetIndex() {
-//		assertEquals(10, list.get(0));
-//	}
-//	@Test
-//	void testIndexOf() {
-//		list.add(3, 10);
-//		assertEquals(0, list.indexOf(10));
-//		assertEquals(-1, list.indexOf(null));
-//	}
-//	
-//	@Test
-//	void testRemovePattern() {
-//		Integer [] expected = { -20, 7, 50, 100, 30};
-//		assertEquals(true, list.remove(new Integer(10)));
-//	}
+	List<Integer> list;
+	Integer[] numbers = {10, -20, 7, 50, 100, 30};
 	
-//	@Test
-//	void testlastIndexOf() {
-//		list.add(3, 10);
-//		assertEquals(3, list.lastIndexOf(10));
-//	}
+	@BeforeEach
+	void setUp() {
+		list = new ArrayList<>(1);
+		for( int i = 0; i < numbers.length; i++) {
+			list.add(numbers[i]);  //только для ar.list система авт(реалокейт*) увелич массив
+		}
+	}
+	
+	@Test
+	void testAdd() {
+	assertTrue(list.add(numbers[0]));
+	assertEquals(numbers.length + 1, list.size());
+	}
+	
+	@Test
+	void testAddIndex() {
+		Integer [] expected0_500 = {500, 10, -20, 7, 50, 100, 30};
+		Integer [] expected0_500_3_700 = {500, 10, -20, 700, 7, 50, 100, 30};
+		Integer [] expected0_500_3_700_8_300 = {500, 10, -20, 700, 7, 50, 100, 30, 300};
+		list.add(0, 500);
+		runTest(expected0_500);
+		list.add(3, 700);
+		runTest(expected0_500_3_700);
+		list.add(8, 300);
+		runTest(expected0_500_3_700_8_300);
+	}
+	
+	@Test
+	void testRemoveIndex() {
+		Integer [] expectedNo10 = { -20, 7, 50, 100, 30};
+		Integer [] expectedNo10_50 = { -20, 7,  100, 30};
+		Integer [] expectedNo10_50_30 = { -20, 7,  100};
+		assertEquals(10, list.remove(0));
+		runTest(expectedNo10);
+		assertEquals(50, list.remove(2));
+		runTest(expectedNo10_50);
+		assertEquals(30, list.remove(3));
+		runTest(expectedNo10_50_30);
+	}
+	
+	@Test
+	void testGetIndex() {
+		assertEquals(10, list.get(0));
+	}
+	
+	@Test
+	void testIndexOf() {
+		list.add(3, 10);
+		assertEquals(0, list.indexOf(10));
+		assertEquals(-1, list.indexOf(null));
+	}
+	
+	@Test
+	void testRemovePattern() {
+		// Passing the Integers this way to make sure the Pattern Function is called - not the Index one
+		assertEquals(true, list.remove(new Integer(10)));
+		assertEquals(false, list.remove(new Integer(999)));
+	}
+	
+	@Test
+	void testlastIndexOf() {
+		list.add(3, 10);
+		assertEquals(3, list.lastIndexOf(10));
+		assertEquals(-1, list.lastIndexOf(999));
+	}
 	
 	@Test
 	void testtoArray() {
 		Integer [] expected = {10, -20, 7, 50, 100, 30, null, null};
-//		Integer [] actual = new Integer[8];
-		try {
-			var actual = list.toArray(new Integer[8]);
-			assertArrayEquals(expected, actual);
-		} catch(Exception e) {
-			System.out.println(e.getMessage());
-		}
-		
-		
+		Object[] actual = list.toArray(new Integer[8]); //Object parent of all
+		assertArrayEquals(expected, actual);
 	}
 	
 	private void runTest(Integer[] expected) {
@@ -95,8 +94,7 @@ void setUp() {
 		for(int i = 0; i < size; i++) {
 			actual[i] = list.get(i);
 		}
-		assertArrayEquals(expected, actual);
-		
+		assertArrayEquals(expected, actual);		
 	}
 
 }
